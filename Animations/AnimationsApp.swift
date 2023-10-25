3d4e1059aa22c3d037cdc1f76697372ef26d91ef
Scott Crowder <scrowder@gmail.com>
2023-10-25 09:13:30 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by SCOTT CROWDER on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
